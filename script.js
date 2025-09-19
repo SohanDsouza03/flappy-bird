ffe0b4845e76b6fbf1be2d93ad7e4abb9a608f0c
SohanDsouza03 <sohandsouza03@gmail.com>
2025-09-19 22:47:28 +0530

    added jump method

--- script.js
+++ script.js
@@ -10,11 +10,56 @@ canvas.height = 600;
 const bird = {
     x: 50,
     y: 150,
-    width: 20,
+    width: 20,                        //<-- bird variable
     height: 20,
     velocity: 0,
     gravity: 0.2
-};
+}; 
+
+function draw() {
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);  //<-- clear canvas in each frame
+
+    
+    ctx.fillStyle = 'red';                                   
+    ctx.fillRect(bird.x, bird.y, bird.width, bird.height);      //<-- draw bird square
+}
+
+function update() {
+    
+    bird.velocity += bird.gravity;
+
+    bird.y += bird.velocity;
+
+    if (bird.y + bird.height > canvas.height) {
+        bird.y = canvas.height - bird.height;
+        bird.velocity = 0;
+    }
+
+    if (bird.y < 0) {
+        bird.y = 0;
+        bird.velocity = 0;
+    }
+}
+
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+function jump() {
+    bird.velocity = -4; // <-- Negative velocity makes it move up
+}
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') {
+        jump();                                 // basically listen to spacebar command
+    }
+});
+
+gameLoop();
 
 // Draw a simple rectangle to test
 ctx.fillStyle = 'red';
